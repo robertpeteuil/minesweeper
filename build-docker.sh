7a2fc0698a6f6bbedbab1fc13d94d5d2096d7527
Robert Peteuil <robert@peteuil.com>
2020-07-21 15:11:00 -0700

    update readme

--- build-docker.sh
+++ build-docker.sh
@@ -8,5 +8,3 @@ set -x
 
 docker run --rm -v ${PWD}:${CONTAINER_PROJECT_PATH} -w ${CONTAINER_PROJECT_PATH} -e CC=/usr/bin/gcc -e GOPATH=/go ${BUILD_IMAGE} sh -c "${BUILD_CMD}"
 docker build -t robpco/minesweeper .
-
-# docker run --rm -p 3000:3000 robpco/minesweeper
