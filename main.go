c4edc7245798ce024784fe1a2a9405eb7f079d8b
Robert Peteuil <robert@peteuil.com>
2020-07-20 17:48:35 -0700

    convert to mod

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/robertpeteuil/minesweeper-api/api"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	//log := &logger{logrus.New()}
+	log := logrus.StandardLogger()
+	log.Infoln("Starting API...")
+	if err := api.Start(log); err != nil {
+		log.WithError(err).Fatalln("Couldn't start API.")
+	}
+}
